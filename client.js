e6bd1499484848784aeaf03d253463d786b6118c
lanmower <almagestfraternite@gmail.com>
2022-12-09 10:12:58 +0200

    Create client.js

---
+++ client.js
@@ -0,0 +1,12 @@
+const DHT = require("@hyperswarm/dht");
+const { base58_to_binary, binary_to_base58 } = require('base58-js');
+const publicKey = base58_to_binary(process.argv[process.argv.length-1])
+const client = async (publicKey) => {
+    const node = new DHT({});
+    await node.ready();
+    console.log('connecting', process.argv[process.argv.length-1]);
+    const socket = node.connect(publicKey);
+    process.stdin.pipe(socket).pipe(process.stdout);
+    console.log('interactive shell connected!');
+}
+client(publicKey);
