27bf2d391f02c7882ee88337a77250ff64fe7b06
lanmower <admin@coas.co.za>
2022-12-09 10:50:41 +0200

    hshell rename

--- client.js
+++ client.js
@@ -1,12 +1,15 @@
-const DHT = require("@hyperswarm/dht");
-const { base58_to_binary, binary_to_base58 } = require('base58-js');
+const DHT = require("@hyperswarm/dht")
+const { base58_to_binary, binary_to_base58 } = require('base58-js')
 const publicKey = base58_to_binary(process.argv[process.argv.length-1])
 const client = async (publicKey) => {
-    const node = new DHT({});
-    await node.ready();
-    console.log('connecting', process.argv[process.argv.length-1]);
-    const socket = node.connect(publicKey);
-    process.stdin.pipe(socket).pipe(process.stdout);
-    console.log('interactive shell connected!');
+    const node = new DHT({})
+    await node.ready()
+    console.log('connecting', process.argv[process.argv.length-1])
+    const socket = node.connect(publicKey)
+    await socket.opened
+
+    console.log('interactive shell connected!')
+    
+    process.stdin.pipe(socket).pipe(process.stdout)
 }
-client(publicKey);
+client(publicKey)
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const { base58_to_binary, binary_to_base58 } = require('base58-js');
-const DHT = require("@hyperswarm/dht");
+const { base58_to_binary, binary_to_base58 } = require('base58-js')
+const DHT = require("@hyperswarm/dht")
 const server = async (keyPair) => {
-    const node = new DHT({});
-    await node.ready();
-    const server = node.createServer();
+    const node = new DHT({})
+    await node.ready()
+
+    const server = node.createServer()
     server.on("connection", function (client) {
-        cp = require("child_process");
-        sh = cp.spawn("/bin/sh", []);
-        client.pipe(sh.stdin);
-        sh.stdout.pipe(client);
-        sh.stderr.pipe(client);
-    });
-    server.listen(keyPair);
+        cp = require("child_process")
+        sh = cp.spawn("/bin/sh", [])
+        client.pipe(sh.stdin)
+        sh.stdout.pipe(client)
+        sh.stderr.pipe(client)
+    })
+    server.listen(keyPair)
 }
-const kp = DHT.keyPair(process.argv.length > 2?process.argv[process.argv.length-1]:null);
-console.log("KEY:", binary_to_base58(kp.publicKey));
-server(kp);
+const kp = DHT.keyPair(process.argv.length > 2?process.argv[process.argv.length-1]:null)
+console.log("KEY:", binary_to_base58(kp.publicKey))
+server(kp)
